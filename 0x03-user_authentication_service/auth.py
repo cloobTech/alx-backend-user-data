7e09dfb7a803d7860f2deb35c583dce289250ee2
CloobTech <belkid98@gmail.com>
2023-08-15 15:26:05 +0100

    simple api

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,9 +3,35 @@
     arguments and returns bytes
 """
 import bcrypt
+from user import User
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """ hashes a pwd"""
-    hashed_pwd = bcrypt.hashpw(password, bcrypt.gensalt())
-    return hashed_pwd.encode('utf-8')
+    hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_pwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user in the DB"""
+        if not email or type(email) != str:
+            return None
+        if not password or type(password) != str:
+            return None
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hashed_pwd = _hash_password(password)
+            user = self._db.add_user(email, hashed_pwd)
+            return user
--- 0x03-user_authentication_service/try.py
+++
@@ -1,8 +0,0 @@
-import bcrypt
-
-def generate_salt_and_hash(password: str) -> bytes:
-    hashed_password = bcrypt.hashpw(password,bcrypt.gensalt(14))
-    return hashed_password.encode('utf-8')
-x = generate_salt_and_hash("hello")
-print(type(x))
-print(x)
