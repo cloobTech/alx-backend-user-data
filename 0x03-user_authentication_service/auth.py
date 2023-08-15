8675bfaa460f0306937b2bc1d48e8c5374ad0003
cloobTech <belkid98@gmail.com>
2023-08-15 10:33:46 +0100

    hash pwd

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,9 @@
 """define a _hash_password method that takes in a password string
     arguments and returns bytes
 """
+import bcrypt
 
 def _hash_password(password: str) -> bytes:
     """ hashes a pwd"""
-    return
+    hashed_pwd = bcrypt.hashpw(password, bcrypt.gensalt())
+    return hashed_pwd.encode('utf-8')
