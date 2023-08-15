4137fad493e283ca72b3901d553bed730c625904
CloobTech <belkid98@gmail.com>
2023-08-15 16:48:40 +0100

    session_id

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 AUTH = Auth()
 
-@app.route("/hi")
+
+@app.route("/")
 def welcome():
     return jsonify({"message": "Bienvenue"})
+
 
 @app.route("/users", methods=['POST'])
 def users():
@@ -16,9 +18,11 @@ def users():
         password = request.form.get('password')
 
         user = AUTH.register_user(email, password)
-        return jsonify({"email": f"{user.email}", "message": "user created"}), 200
+        return_val = {"email": f"{user.email}", "message": "user created"}
+        return jsonify(return_val), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,9 +3,11 @@
     arguments and returns bytes
 """
 import bcrypt
+import uuid
 from user import User
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 def _hash_password(password: str) -> bytes:
@@ -24,9 +26,9 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """Register a user in the DB"""
         if not email or type(email) != str:
-            return None
+            raise InvalidRequestError("Invalid email format")
         if not password or type(password) != str:
-            return None
+            raise InvalidRequestError("Invalid password format")
         try:
             user = self._db.find_user_by(email=email)
             if user:
@@ -35,3 +37,36 @@ class Auth:
             hashed_pwd = _hash_password(password)
             user = self._db.add_user(email, hashed_pwd)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """check if a login is valid"""
+        if not email or type(email) != str:
+            raise InvalidRequestError("Invalid email format")
+        if not password or type(password) != str:
+            raise InvalidRequestError("Invalid password format")
+        try:
+            user = self._db.find_user_by(email=email)
+            hashed_pwd = user.hashed_password
+            if bcrypt.checkpw(password.encode('utf-8'), hashed_pwd):
+                return True
+        except NoResultFound:
+            pass
+        return False
+
+    def _generate_uuid(self) -> str:
+        """Generate a new UUID string."""
+        return str(uuid.uuid4())
+
+    def create_session(self, email: str) -> str:
+        if not email or type(email) != str:
+            raise InvalidRequestError("Invalid email format")
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = self._generate_uuid()
+            user_id = user.id
+            self._db.update_user(user_id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            raise ('Invalid User')
+
+
