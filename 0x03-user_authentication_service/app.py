b1e30a822b02aa390068f089ea8dcdffefff7fd6
CloobTech <belkid98@gmail.com>
2023-08-16 16:44:36 +0100

    fixed "/" route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,14 +2,15 @@ from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 from sqlalchemy.orm.exc import NoResultFound
 
-app = Flask(__name__)
-
 AUTH = Auth()
+app = Flask(__name__)
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
 def index():
-    """Welcome route - index page of the app"""
+    """
+    Welcome route - index page of the app
+    """
     return jsonify({"message": "Bienvenue"})
 
 
